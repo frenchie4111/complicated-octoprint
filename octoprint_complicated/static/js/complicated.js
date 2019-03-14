6ad533ede046ee3ac7215ee0894e81f0b3dc77cc
Michael Lyons <mdl0394@gmail.com>
2019-03-13 20:35:36 -0700

    Initial Commit

---
+++ octoprint_complicated/static/js/complicated.js
@@ -0,0 +1,29 @@
+/*
+ * View model for OctoPrint-Complicated
+ *
+ * Author: Mike Lyons
+ * License: AGPLv3
+ */
+$(function() {
+    function ComplicatedViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: ComplicatedViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_complicated, #tab_plugin_complicated, ...
+        elements: [ /* ... */ ]
+    });
+});
