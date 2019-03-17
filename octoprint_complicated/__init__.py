25d3684bc459b7cd5145264180a8d50b512cf472
Michael Lyons <mdl0394@gmail.com>
2019-03-17 12:29:56 -0700

    Fixed settings stuff to use new key

--- octoprint_complicated/__init__.py
+++ octoprint_complicated/__init__.py
@@ -18,16 +18,13 @@ import octoprint.plugin
 class ComplicatedPlugin( octoprint.plugin.ProgressPlugin, octoprint.plugin.SettingsPlugin, octoprint.plugin.TemplatePlugin ):
     config_version_key = '1'
 
-    def __init__( self ):
-        pass
-
     # #~~ SettingsPlugin mixin
 
     def get_settings_defaults( self ):
         return dict(
-            api_key="",
+            api_key="Put Your Api Key Here",
             selected_complication="modularLarge",
-            value_template="Print {progress} Completed"
+            value_template="Print {progress}% Completed"
         )
 
     # #~~ 
@@ -45,8 +42,8 @@ class ComplicatedPlugin( octoprint.plugin.ProgressPlugin, octoprint.plugin.Setti
         self._logger.info( 'Updating server with progress {}'.format( progress ) )
 
         api_key = self._settings.get( [ 'api_key' ] )
-        if api_key == "" or api_key == None:
-            self._logger.info( 'Api Key Not Set' )
+        if api_key == "" or api_key == None or len( api_key ) != 8:
+            self._logger.info( 'Api Key Not Set, or Invalid' )
             return
 
         selected_complciation = self._settings.get( [ 'selected_complication' ] )
@@ -63,27 +60,6 @@ class ComplicatedPlugin( octoprint.plugin.ProgressPlugin, octoprint.plugin.Setti
 
         complicated_lib.changeComplication( api_key, selected_complciation, new_value )
 
-    # #~~ Softwareupdate hook
-
-    def get_update_information(self):
-        # Define the configuration for your plugin to use with the Software Update
-        # Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
-        # for details.
-        return dict(
-            complicated = dict( 
-                # version check: github repository
-                # update method: pip
-                displayName='Complicated Plugin',
-                displayVersion=self._plugin_version,
-                type='github_release',
-                user='frenchie4111',
-                repo='complicated-octoprint',
-                current=self._plugin_version,
-                pip='https://github.com/frenchie4111/complicated-octoprint/archive/{target_version}.zip',
-            )
-        )
-
-
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
@@ -91,4 +67,4 @@ class ComplicatedPlugin( octoprint.plugin.ProgressPlugin, octoprint.plugin.Setti
 __plugin_name__ = 'Complicated Plugin'
 __plugin_version__ = '1.0.0'
 __plugin_implementation__ = ComplicatedPlugin()
-__plugin_hooks__ = { 'octoprint.plugin.softwareupdate.check_config': __plugin_implementation__.get_update_information }
+__plugin_hooks__ = {}
